7ea60e645f129e2cf80e270ae0920bae0d336be5
Johnathan Voss <JVoss11@my.wctc.edu>
2023-03-21 20:01:52 -0500

    Having some issues with numbers 7+8, can't seem to get them just right.

--- StringInterpolationTemplate/Services/StringInterpolationService.cs
+++ StringInterpolationTemplate/Services/StringInterpolationService.cs
@@ -26,54 +26,77 @@ public class StringInterpolationService : IStringInterpolationService
         return answer;
     }
 
+    //2 - 2019.01.22
     public string Number02()
     {
-        throw new NotImplementedException();
+        var today = _date.Now;
+        return $"{today:yyyy.MM.dd}";
     }
 
+    //3 - Day 22 of January, 2019
     public string Number03()
     {
-        throw new NotImplementedException();
+        var today = _date.Now;
+        return $"Day {today:dd} of {today:MMMM, yyyy}";
     }
 
+    //4 - Year: 2019, Month: 01, Day: 22
     public string Number04()
     {
-        throw new NotImplementedException();
+        var today = _date.Now;
+        return $"Year: {today:yyyy}, Month: {today:mm}, Day: {today:dd}";
     }
 
+    //5 -            Tuesday (10 spaces total, right aligned)
     public string Number05()
     {
-        throw new NotImplementedException();
+        var today = _date.Now;
+        return $"{today,10:dddd}";
     }
 
+    //6 -     11:01 PM             Tuesday (10 spaces total for each including the day-of-week, both right-aligned)
     public string Number06()
     {
-        throw new NotImplementedException();
+        var today = _date.Now;
+        return $"{today,10:t}{today,10:dddd}";
     }
 
+    //7 - h:11, m:01, s:27
     public string Number07()
     {
-        throw new NotImplementedException();
+        var today = _date.Now;
+        return $"h:{today.Hour}, m:{today.Minute}, s:{today.Second}";
     }
 
+    //8 - 2019.01.22.11.01.27
     public string Number08()
     {
-        throw new NotImplementedException();
+        var today = _date.Now;
+        return $"{today:yyyy.MM.dd.}{today.Hour}.{today.Minute}.{today.Second}";
     }
 
+    // NUMBER TIME
+    // If you have PI (3.1415) - use var pi = Math.PI;
+
+    //1 - Output as currency
     public string Number09()
     {
-        throw new NotImplementedException();
+        var pi = Math.PI;
+        return $"{pi:C2}";
     }
 
+    //2 - Output as right-aligned (10 spaces), number with 3 decimal places
     public string Number10()
     {
-        throw new NotImplementedException();
+        var pi = Math.PI;
+        return $"{pi,10:N3}";
     }
 
     public string Number11()
     {
-        throw new NotImplementedException();
+        var today = _date.Now;
+
+        return $"{today.Year.ToString("X")}";
     }
 
     //2.2019.01.22
